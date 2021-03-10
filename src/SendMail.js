a0208b1a5f8067f2e17f40657a76f6cd4285d136
Deepak <deepsrh1801@gmail.com>
2021-03-10 17:57:18 +0530

    added error fields

--- src/SendMail.js
+++ src/SendMail.js
@@ -2,22 +2,45 @@ import React from "react";
 import "./SendMail.css";
 import CloseIcon from "@material-ui/icons/Close";
 import { Button } from "@material-ui/core";
+import { useForm } from "react-hook-form";
 
 function SendMail() {
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className="sendMail">
       <div className="sendMail__header">
         <h3>New Message</h3>
         <CloseIcon className="sentMail__close" />
       </div>
-      <form>
-        <input type="text" placeholder="To" />
-        <input type="text" placeholder="Subject" />
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
+          name="to"
+          type="text"
+          placeholder="To"
+          ref={register({ required: true })}
+        />
+        {errors.to && <p className="sendMail__error">required</p>}
+        <input
+          name="subject"
+          type="text"
+          placeholder="Subject"
+          ref={register({ required: true })}
+        />
+        {errors.subject && <p className="sendMail__error">required</p>}
+
+        <input
+          name="message"
           type="text"
           placeholder="Message..."
           className="sendMail__message"
+          ref={register({ required: true })}
         />
+        {errors.message && <p className="sendMail__error">required</p>}
+
         <div className="sendMail__options">
           <Button
             className="sendMail__send"
