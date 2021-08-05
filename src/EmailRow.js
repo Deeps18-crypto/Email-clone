95c1c782f00de7ecc7cb20cdbfb544b7e5ba64f0
Deepak <deepsrh1801@gmail.com>
2021-08-05 22:48:27 +0530

    responsed the body page

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -6,6 +6,20 @@ import "./EmailRow.css";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { selectMail } from "./features/counter/MailSlice";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(1.5),
+    [theme.breakpoints.down("sm")]: {
+      display:"block"
+    },
+    [theme.breakpoints.up("md")]: {
+      backgroundColor: theme.palette.primary.main,
+    },
+    [theme.breakpoints.up("lg")]: {},
+  },
+}));
 
 function EmailRow({ title, description, id, subject, time, message }) {
   const history = useHistory();
@@ -23,8 +37,9 @@ function EmailRow({ title, description, id, subject, time, message }) {
     );
     history.push("/mail");
   };
+  const classes = useStyles();
   return (
-    <div onClick={openMail} className="emailRow">
+    <div onClick={openMail} className="emailRow" className={classes.root}>
       <Hidden only={["sm", "xs"]}>
         <div className="emailRow__options">
           <Checkbox />
@@ -43,7 +58,9 @@ function EmailRow({ title, description, id, subject, time, message }) {
           <spam className="emailRow__description">- {description}</spam>
         </h4>
       </div>
-      <div className="emailRow__time">{time}</div>
+      <Hidden only={["sm", "xs"]}>
+        <div className="emailRow__time">{time}</div>
+      </Hidden>
     </div>
   );
 }
